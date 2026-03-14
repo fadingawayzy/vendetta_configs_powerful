2b1188ec257fdba0cfa95441a051c34350ea7500
fadingawayzy <diediediemydarling93@gmail.com>
2026-03-14 11:42:36 +0300

    button moves

--- app/bot/keyboards/main_kb.py
+++ app/bot/keyboards/main_kb.py
@@ -16,6 +16,8 @@ def get_inline_menu():
     builder = InlineKeyboardBuilder()
     builder.button(text="♾ Моя Подписка", callback_data="subs_menu")
 
+    builder.button(text="📱 Sing-Box", callback_data="singbox_menu")
+
     builder.button(text="🌍 Выбор страны", callback_data="country_menu")
 
     builder.button(text="📚 FAQ / Помощь", url="https://telegra.ph/Instrukciya-po-podklyucheniyu-VPN-cherez-VLESS-01-11")
@@ -23,7 +25,5 @@ def get_inline_menu():
     builder.button(text="🎲 Мне повезет", callback_data="lucky")
 
     builder.button(text="🤝 Поделиться", callback_data="share_menu")
-
-    builder.button(text="📱 Sing-Box", callback_data="singbox_menu")
     builder.adjust(1)
     return builder.as_markup()
